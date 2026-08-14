a50659fec09be50881fc1a36b1bf442525da8de4
Dvice <70948113+dvicewashere@users.noreply.github.com>
2026-08-14 17:18:12 +0300

    Update BASLAT.bat

--- BASLAT.bat
+++ BASLAT.bat
@@ -3,7 +3,7 @@ title TUBITAK Atif Dogrulayici
 chcp 65001 > nul
 echo.
 echo ========================================================
-echo   TÜBİTAK Atıf Doğrulayıcı Başlatılıyor... Dvice selamlarını iletiyooor.
+echo   TÜBİTAK Atıf Doğrulayıcı Başlatılıyor...
 echo ========================================================
 echo.
 
